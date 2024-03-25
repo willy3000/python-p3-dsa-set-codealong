fc9509dc58852bab0bf285db44b19160df7be737
Jillo Wario <willywario0@gmail.com>
2024-03-25 08:59:48 +0300

    Done with assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,16 @@
 class MySet:
-
-    pass
+    def __init__(self, list = []):
+        self.dictionary = {}
+        for value in list:
+           self.dictionary[value] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+       self.dictionary[value] = True 
+       return self
+    
+    def delete(self, value):
+      self.dictionary.pop(value, None)
+      return self
